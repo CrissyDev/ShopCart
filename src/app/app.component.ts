a0ee86467fb2857bd1c400a00472ad1ee676bcd3
Crissy <christalriziki@gmail.com>
2025-05-27 15:56:51 +0300

    delivery component complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { ProductListComponent } from "./product-list/product-list.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     RouterOutlet,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent
 ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'] 
--- src/app/delivery/delivery.component.ts
+++ src/app/delivery/delivery.component.ts
@@ -1,11 +1,29 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-delivery',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule], 
   templateUrl: './delivery.component.html',
-  styleUrl: './delivery.component.css'
+  styleUrls: ['./delivery.component.css']
 })
 export class DeliveryComponent {
-
+  features = [
+    {
+      title: 'Robots-driver',
+      description: 'Our mini-robots move autonomously and ensure fast delivery to any part of the city.',
+      icon: 'fas fa-robot'
+    },
+    {
+      title: 'Online delivery ordering',
+      description: 'Order from anywhere in the city using our efficient online system.',
+      icon: 'fas fa-laptop'
+    },
+    {
+      title: 'Support Team',
+      description: 'Our team is available 24/7 to support your delivery needs.',
+      icon: 'fas fa-headset'
+    }
+  ];
 }
