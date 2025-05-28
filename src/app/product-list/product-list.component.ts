151e4c4f0ee1cb5c1f9e4a44090c7c9378847a56
Crissy <christalriziki@gmail.com>
2025-05-28 12:01:42 +0300

    added some functionalities

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, inject, OnInit } from '@angular/core';
-import { NgClass, NgFor } from '@angular/common';
-import { RouterModule, ActivatedRoute } from '@angular/router';
+import { NgClass, NgIf, NgFor, CommonModule } from '@angular/common';
+import { RouterModule, ActivatedRoute, Router } from '@angular/router';
 import { ProductService } from '../services/product.service';
 import { Product } from '../models/product.interface';
 import { SearchService } from '../services/search.service';
@@ -8,20 +8,20 @@ import { SearchService } from '../services/search.service';
 @Component({
   selector: 'app-product-list',
   standalone: true,
-  imports: [NgClass, RouterModule],
+  imports: [CommonModule, NgClass, NgIf, RouterModule],
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css']
 })
 export class ProductListComponent implements OnInit {
-productDetail(arg0: number) {
-throw new Error('Method not implemented.');
-}
   private productService = inject(ProductService);
   private searchService = inject(SearchService);
   private route = inject(ActivatedRoute);
+  private router = inject(Router);
 
   products: Product[] = [];
   filteredProducts: Product[] = [];
+  visibleProducts: Product[] = [];
+  visibleCount: number = 8;
 
   ngOnInit(): void {
     this.productService.getProducts().subscribe((res) => {
@@ -32,7 +32,6 @@ throw new Error('Method not implemented.');
         rating: parseFloat((Math.random() * 2 + 3).toFixed(1))
       }));
 
-      
       this.route.queryParams.subscribe(params => {
         const category = params['category'];
         if (category) {
@@ -42,6 +41,8 @@ throw new Error('Method not implemented.');
         } else {
           this.filteredProducts = [...this.products];
         }
+
+        this.updateVisibleProducts();
       });
     });
 
@@ -51,6 +52,8 @@ throw new Error('Method not implemented.');
         product.title.toLowerCase().includes(lowerTerm) ||
         product.description.toLowerCase().includes(lowerTerm)
       );
+      this.visibleCount = 8;
+      this.updateVisibleProducts();
     });
   }
 
@@ -58,7 +61,21 @@ throw new Error('Method not implemented.');
     product.liked = !product.liked;
   }
 
-//   productDetail(id){
-// this.router
-//   }
+  productDetail(id: number): void {
+    this.router.navigate(['/product', id]);
+  }
+
+  seeMore(): void {
+    this.visibleCount = this.filteredProducts.length;
+    this.updateVisibleProducts();
+  }
+
+  seeLess(): void {
+    this.visibleCount = 8;
+    this.updateVisibleProducts();
+  }
+
+  private updateVisibleProducts(): void {
+    this.visibleProducts = this.filteredProducts.slice(0, this.visibleCount);
+  }
 }
